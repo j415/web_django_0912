aadc3ce4cf47f5aec0730329d91afa6303bcfb26
j415 <1720049083@qq.com>
2018-09-12 20:53:11 +0800

    add ablog project

---
+++ aspblog/aspblog/urls.py
@@ -0,0 +1,26 @@
+"""aspblog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from myblog.views import IndexView, ArichiveView,TagView,TagDetailView
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^archive/$', ArichiveView.as_view(), name='archive'),
+    url(r'^tags/$',TagView.as_view(),name='tags'),
+    url(r'^tags/(?P<tag_name>\w+)$',TagDetailView.as_view(),name='tag_name'),
+]
